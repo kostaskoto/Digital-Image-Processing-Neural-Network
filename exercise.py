83732cc5ed60973e439d813277856a71b8826180
kostaskoto <kostas123koto@hotmail.gr>
2023-06-28 22:18:26 +0300

    changed some variables

--- exercise.py
+++ exercise.py
@@ -2,18 +2,19 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import idx2numpy
-import math
+
+
 
 file = './data/train-images.idx3-ubyte'
 legend = './data/train-labels.idx1-ubyte'
-arr = idx2numpy.convert_from_file(file)
-arr2 = idx2numpy.convert_from_file(legend)
+imgsTraining = idx2numpy.convert_from_file(file)
+labelsTraning = idx2numpy.convert_from_file(legend)
 
 f, imgplot = plt.subplots(10,10)
 
 for i in range(10):
     for j in range(10):
-        imgplot[i,j].imshow(arr[i*10+j], cmap='gray')
-        imgplot[i,j].text(1, 1, arr2[i*10+j], bbox={'facecolor': 'white'})
+        imgplot[i,j].imshow(imgsTraining[i*10+j], cmap='gray')
+        imgplot[i,j].text(1, 1, labelsTraning[i*10+j], bbox={'facecolor': 'white'})
 
 plt.show()
