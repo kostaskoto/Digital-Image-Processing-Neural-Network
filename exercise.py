e8edf8a6c98300f085fb20a423f6e473136947ad
kostaskoto <kostas123koto@hotmail.gr>
2023-06-30 09:29:55 +0300

    part1 ex 1 done

--- exercise.py
+++ exercise.py
@@ -4,17 +4,43 @@ import numpy as np
 import idx2numpy
 
 
+# def stohasticGradiendDescent(X, Y, W, b, learningRate, epochs):
+#     for epoch in range(epochs):
+#         for i in range(X.shape[0]):
+#             x = X[i]
+#             y = Y[i]
+#             z = torch.dot(W, x) + b
+#             yHat = sigmoid(z)
+#             loss = -(y * torch.log(yHat) + (1 - y) * torch.log(1 - yHat))
+#             loss.backward()
+#             W = W - learningRate * W.grad
+#             b = b - learningRate * b.grad
+#             W.grad.zero_()
+#             b.grad.zero_()
+#     return W, b
 
 file = './data/train-images.idx3-ubyte'
 legend = './data/train-labels.idx1-ubyte'
 imgsTraining = idx2numpy.convert_from_file(file)
 labelsTraning = idx2numpy.convert_from_file(legend)
 
-f, imgplot = plt.subplots(10,10)
+f, imgplot = plt.subplots(1,10)
+lstImg = []
+lstLabel = []
 
-for i in range(10):
-    for j in range(10):
-        imgplot[i,j].imshow(imgsTraining[i*10+j], cmap='gray')
-        imgplot[i,j].text(1, 1, labelsTraning[i*10+j], bbox={'facecolor': 'white'})
+for i in range(len(labelsTraning)):
+    if not labelsTraning[i] in lstLabel:
+        lstImg.append(imgsTraining[i])
+        lstLabel.append(labelsTraning[i])
+    if len(lstImg) == 10:
+        break
+
+sort = np.argsort(lstLabel)
+lstImg = np.array(lstImg)[sort]
+lstLabel = np.array(lstLabel)[sort]
+
+for i in range(len(lstImg)):       
+    imgplot[i].imshow(lstImg[i], cmap='gray')
+    imgplot[i].text(1, 1, lstLabel[i], bbox={'facecolor': 'white'})
 
 plt.show()
